c7a3f9e06177e08f6770ad082682baa39a638ebb
dimontttt <olga.dimont@yandex.ru>
2024-03-29 21:19:39 +0300

    Добавьте файлы проекта.

---
+++ lab3.cpp
@@ -0,0 +1,42 @@
+﻿// lab3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void countingSort(vector<int>& arr, int n, int m) 
+{
+	vector<int> count(m + 1, 0);
+	int index = 0;
+
+	for (int i = 0; i < n; i++)
+		count [arr[i]]++;
+		
+	for (int i = 0; i <= m; i++)
+		while (count[i] > 0) 
+		{
+			arr[index] = i;
+			index++;
+			count[i]--;
+		}
+	
+}
+
+int main() 
+{
+	setlocale(LC_ALL, "rus");
+	vector<int> arr = { 4, 2, 2, 8, 3, 3, 1 };
+	int n = arr.size();
+	int m = 8;
+	for (int i = 0; i < n; i++)
+		cout << " " << arr[i];
+
+	countingSort(arr, n, m);
+
+	cout << "\nОтсортированный массив:\n";
+	for (int i = 0; i < n; i++)
+		cout << " " << arr[i];
+
+	return 0;
+}
